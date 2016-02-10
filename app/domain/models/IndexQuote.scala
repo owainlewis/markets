cf1ce92a64c91c4720f498bc354f09cfcb66abff
Owain Lewis <owain@owainlewis.com>
2016-02-10 18:59:56 +0000

    Update IndexQuote.scala

--- app/domain/models/IndexQuote.scala
+++ app/domain/models/IndexQuote.scala
@@ -3,10 +3,15 @@ package domain.models
 import io.forward.yahoo.YahooStockResponse
 import play.api.libs.json.Json
 
-case class IndexQuote(name: String, symbol: String, price: Double, change: Double, dayHigh: Double, dayLow: Double)
+case class IndexQuote(
+  name: String, 
+  symbol: String, 
+  price: Double, 
+  change: Double, 
+  dayHigh: Double, 
+  dayLow: Double)
 
 object IndexQuote {
-
   implicit val format = Json.format[IndexQuote]
 
   def fromYahoo(quote: YahooStockResponse): IndexQuote =
